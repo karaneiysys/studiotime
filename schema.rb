9e91a25a5c6919c33f60a8da726dfaf0e7a91c4f
karaneiysys <50905823+karaneiysys@users.noreply.github.com>
2019-05-22 14:32:31 +0530

    Add files via upload

--- schema.rb
+++ schema.rb
@@ -10,40 +10,50 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_17_103507) do
+ActiveRecord::Schema.define(version: 2019_02_06_064529) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "booking_dates", force: :cascade do |t|
-    t.bigint "booking_id"
-    t.string "date"
-    t.string "start_time"
-    t.string "end_time"
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["booking_id"], name: "index_booking_dates_on_booking_id"
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "studio_id"
-    t.integer "subtotal"
-    t.integer "studiotime_fee"
-    t.integer "total"
-    t.boolean "confirmed", default: false
+    t.integer "user_id"
+    t.integer "studio_id"
+    t.integer "status", null: false
+    t.text "message"
+    t.string "card_holder_name", null: false
+    t.string "country", null: false
+    t.text "billing_address", null: false
+    t.string "postal_code", null: false
+    t.string "city", null: false
+    t.string "state"
+    t.string "card_number"
+    t.string "expiry_month"
+    t.string "expiry_year"
+    t.string "cvv"
+    t.float "sub_total", null: false
+    t.float "studiotime_fee", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["studio_id"], name: "index_bookings_on_studio_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
-  end
-
-  create_table "photos", force: :cascade do |t|
-    t.string "photo"
-    t.bigint "studio_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["studio_id"], name: "index_photos_on_studio_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -57,67 +67,80 @@ ActiveRecord::Schema.define(version: 2019_01_17_103507) do
     t.index ["reviewer_type", "reviewer_id"], name: "index_reviews_on_reviewer_type_and_reviewer_id"
   end
 
-  create_table "studio_types", force: :cascade do |t|
-    t.bigint "studio_id"
-    t.bigint "type_id"
+  create_table "slots", force: :cascade do |t|
+    t.integer "booking_id"
+    t.date "date", null: false
+    t.time "from", null: false
+    t.time "to", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["studio_id"], name: "index_studio_types_on_studio_id"
-    t.index ["type_id"], name: "index_studio_types_on_type_id"
+    t.index ["booking_id"], name: "index_slots_on_booking_id"
+  end
+
+  create_table "studio_types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "studio_types_studios", id: false, force: :cascade do |t|
+    t.integer "studio_id", null: false
+    t.integer "studio_type_id", null: false
+    t.index ["studio_id", "studio_type_id"], name: "index_studio_types_studios_on_studio_id_and_studio_type_id"
+    t.index ["studio_type_id", "studio_id"], name: "index_studio_types_studios_on_studio_type_id_and_studio_id"
   end
 
   create_table "studios", force: :cascade do |t|
-    t.bigint "user_id"
-    t.string "name"
-    t.string "country"
-    t.string "city"
-    t.string "latitude"
-    t.string "longitude"
-    t.string "price"
-    t.boolean "verified", default: false
-    t.boolean "premium", default: false
-    t.boolean "quick_replies", default: false
-    t.boolean "audio_eng_included", default: false
-    t.integer "rating"
-    t.text "description"
-    t.integer "min_booking"
-    t.text "main_equipment"
+    t.integer "user_id"
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "min_booking_time", null: false
     t.text "past_clients"
     t.text "audio_samples"
-    t.string "hours"
-    t.text "rules"
+    t.time "opening_time", null: false
+    t.integer "studio_type_id"
+    t.time "closing_time", null: false
+    t.text "amenities", null: false
+    t.text "main_equipment", null: false
+    t.text "rules", null: false
+    t.text "address", null: false
+    t.string "landmark", default: ""
+    t.string "lat", null: false
+    t.string "lng", null: false
+    t.float "price_per_hour", null: false
+    t.boolean "aud_eng_included", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["audio_eng_included"], name: "index_studios_on_audio_eng_included"
-    t.index ["city"], name: "index_studios_on_city"
-    t.index ["country"], name: "index_studios_on_country"
-    t.index ["premium"], name: "index_studios_on_premium"
-    t.index ["quick_replies"], name: "index_studios_on_quick_replies"
+    t.index ["studio_type_id"], name: "index_studios_on_studio_type_id"
     t.index ["user_id"], name: "index_studios_on_user_id"
-    t.index ["verified"], name: "index_studios_on_verified"
-  end
-
-  create_table "types", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "password_digest"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "phone_no"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.text "bio", default: ""
+    t.string "website", default: ""
+    t.string "facebook_handle", default: ""
+    t.string "twitter_handle", default: ""
+    t.string "insta_handle", default: ""
+    t.string "stream_url", default: ""
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.string "mobile", default: ""
     t.boolean "email_verified", default: false
-    t.boolean "phone_no_verified", default: false
-    t.text "description"
-    t.string "response_rate"
-    t.string "response_time"
-    t.string "url"
-    t.string "profile_pic"
+    t.boolean "mobile_verified", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "verifications", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "token"
+    t.datetime "expires_at"
+    t.integer "kind"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_verifications_on_user_id"
   end
 
 end
